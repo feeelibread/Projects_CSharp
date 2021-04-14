47b87ec9244c19c215f0a370a113c5c576e251f2
feeelibread <feeelpoos@gmail.com>
2021-04-14 11:27:40 -0300

    Add project files.

---
+++ Projects_CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While_Structure_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int password = int.Parse(Console.ReadLine());
+
+            while (password != 2002) 
+            {
+                Console.WriteLine("Senha inválida!");
+                password = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido!");
+
+
+        }
+
+    }
+}
