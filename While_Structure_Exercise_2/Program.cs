6bd7356c40bec439418a7a4374f395d4866b4ea2
feeelibread <feeelpoos@gmail.com>
2021-04-14 12:15:06 -0300

    An exercise about "While" structure.

---
+++ While_Structure_Exercise_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace While_Structure_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] coordenates = Console.ReadLine().Split(' ');
+            int x = int.Parse(coordenates[0]);
+            int y = int.Parse(coordenates[1]);
+
+            while (x != 0 || y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                }
+                
+                coordenates = Console.ReadLine().Split(' ');
+                x = int.Parse(coordenates[0]);
+                y = int.Parse(coordenates[1]);
+            }
+
+        }
+    }
+}
