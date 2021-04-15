ba3793b5c986ec08ac9e0df955f7074a7ca13803
feeelibread <feeelpoos@gmail.com>
2021-04-15 15:14:26 -0300

    Anooooother problem resolved with OOP (i forgot to say that it's just the basic of it... For now at least)

---
+++ Problem_With_OOP_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem_With_OOP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a Altura e Largura do retângulo:");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(r);
+
+        }
+    }
+}
---
+++ Problem_With_OOP_3/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem_With_OOP_3
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"AREA = {Area().ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"PERMIETRO = {Perimetro().ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"DIAGONAL = {Diagonal().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
