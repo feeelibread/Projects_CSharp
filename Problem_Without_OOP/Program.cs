fec869b692e7b269a0c01e8dd4aa8f8dd172508b
feeelibread <feeelpoos@gmail.com>
2021-04-15 11:52:14 -0300

    Problem resolved with OOP

--- Problem_Without_OOP/Program.cs
+++ Problem_Without_OOP/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Problem_Without_OOP
+namespace Problem_With_OOP
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Triangle x, y;
+            x = new Triangle();
+            y = new Triangle();
+
             Console.WriteLine("Entre com as medidas do triângulo X:");
-            double xA, xB, xC, yA, yB, yC;
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine("Entre com a medida do triângulo Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p- xC));
-            p = (yA + yB + yC) / 2;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
-
+            double areaX = x.Area();
+            double areaY = y.Area();
             Console.WriteLine($"Área do triângulo X: {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Área do triângulo Y: {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
 
---
+++ Problem_Without_OOP/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem_With_OOP
+{
+    class Triangle
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p;
+            p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            
+        }
+    }
+}
