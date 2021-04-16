666f3ef39899a95e995bb034731e354beffc88a9
feeelibread <feeelpoos@gmail.com>
2021-04-16 15:25:04 -0300

    Well... as the name says, it's another problem resolved with OOP  (I know that I need to be creative with names)

---
+++ Problem_With_OOP_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem_With_OOP_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          
+            Console.WriteLine("\nFuncionário: " + func);
+
+            Console.Write("\nDigite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+
+            Console.WriteLine("\nDados atualizados: " + func);
+
+        }
+    }
+}
