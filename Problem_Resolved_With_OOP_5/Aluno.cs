7453c880e139bd9c9b19dd4c6e657bcb9816fbc0
feeelibread <feeelpoos@gmail.com>
2021-04-16 15:39:56 -0300

    You might actually know what is this about... right?

---
+++ Problem_Resolved_With_OOP_5/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem_Resolved_With_OOP_5
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public override string ToString()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return $"NOTA FINAL = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}\nAPROVADO!";
+            }
+            else
+            {
+                double restante = 60 - NotaFinal();
+                return $"NOTA FINAL = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}" +
+                    $"\nREPROVADO!" +
+                    $"\nRESTARAM {restante} PONTOS!";
+            }
+         }
+    }
+}
---
+++ Problem_Resolved_With_OOP_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem_Resolved_With_OOP_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.Write("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(aluno);
+
+        }
+    }
+}
