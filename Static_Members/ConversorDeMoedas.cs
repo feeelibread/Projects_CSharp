13d7f037a30a35c9e842c4fa5d9b3aa996d0b494
feeelibread <feeelpoos@gmail.com>
2021-04-19 17:01:08 -0300

    I've created this class to test how to work a "Static" attribute, I learned a lot about it.

---
+++ Static_Members/ConversorDeMoedas.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Static_Members
+{
+    class ConversorDeMoedas
+    {
+        public static double IOF = 0.06;
+        public double CotacaoDolar;
+        public double ValorEmDolar;
+
+        public double ValorEmReais()
+        {
+            double result = ValorEmDolar * CotacaoDolar;
+            return  result + (result * IOF);
+        }
+
+        public override string ToString()
+        {
+            return $"Valor a ser pago em reais = {ValorEmReais().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Static_Members/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Static_Members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeMoedas conversor = new ConversorDeMoedas();
+
+            Console.Write("Qual é a cotação do dólar? ");
+            conversor.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            conversor.ValorEmDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(conversor);
+
+        }
+    }
+}
