853c755d71efa1cf8b1d7b1a41e7678fd0338d3b
feeelibread <feeelpoos@gmail.com>
2021-04-26 10:01:32 -0300

    Studies about type Struct

---
+++ Struct_Types/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Struct_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+        }
+    }
+}
