9adfd28ce89dd89f3b7bd647c7293aa9556922ee
feeelibread <feeelpoos@gmail.com>
2021-04-27 10:23:50 -0300

    Nullable functions.

---
+++ Nullable_Types/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nullable_Types_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //struct types cannot be null like.
+            //double x = null; That's the wrong way, but we can make this happen like the example below. The easier way or... the hardest one?
+            
+            Nullable<double> x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value); 
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine("Y is not null: " + y.Value);
+            }
+
+
+
+        }
+    }
+}
---
+++ Nullable_Types_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nullable_Types_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10;
+
+            double a = y ?? 5.0;
+            double b = x ?? 5.0;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
