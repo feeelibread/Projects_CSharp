f118ee27beec18c64b38b08c418f0d4fd89ee6bd
feeelibread <feeelpoos@gmail.com>
2021-04-27 12:15:16 -0300

    Summing the elements of the vector.

---
+++ Vect_Exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vect_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+
+            double sum = 0.0;
+            for(int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                sum += vect[i];
+            }
+            sum = sum / n;
+            Console.WriteLine("AVERAGE HEIGHT = " + sum.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
