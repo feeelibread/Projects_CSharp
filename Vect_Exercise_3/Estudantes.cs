df9b29e4cd7702edfa8d8d8f6740b83cefbc1534
feeelibread <feeelpoos@gmail.com>
2021-05-03 11:57:50 -0300

    The most difficult exercise about vect that i've done until now.

---
+++ Vect_Exercise_3/Estudantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vect_Exercise_3
+{
+    class Estudantes
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+
+    }
+}
---
+++ Vect_Exercise_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vect_Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] students = new Estudantes[10];
+            Console.Write("How many rooms will be rented? ");
+            int rent = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= rent; i++)
+            {
+                Console.WriteLine($"\nRent #{i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                students[room] = new Estudantes(name, email);
+            }
+
+            Console.WriteLine("\nBusy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (students[i] != null)
+                {
+                    Console.WriteLine($"{i}: {students[i]}");
+                }
+
+            }
+        }
+    }
+}
