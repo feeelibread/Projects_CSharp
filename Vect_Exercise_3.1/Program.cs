6e19f6d59b17a37d0f7c62011c52057ac2890025
feeelibread <feeelpoos@gmail.com>
2021-05-06 10:21:13 -0300

    Basically i was remaking the previous Exercise about vectors, just to make sure that i learned it. But i need to review that.

---
+++ Vect_Exercise_3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Vect_Exercise_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students[] students = new Students[10];
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nRent #{i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                students[room] = new Students(name, email);
+            }
+
+            Console.WriteLine("\nBusy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if(students[i] != null)
+                    Console.WriteLine($"{i}: {students[i]}");
+            }
+
+        }
+    }
+}
---
+++ Vect_Exercise_3.1/Students.cs
@@ -0,0 +1,19 @@
+﻿namespace Vect_Exercise_3._1
+{
+    class Students
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Students(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
