3ab2c9dba24b3e2d12a070fb208ad5253c7ff2ec
feeelibread <feeelpoos@gmail.com>
2021-05-10 10:36:14 -0300

    Learning about ref, that's a solution that doesn't work.

---
+++ Calculator_2/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator_2
+{
+    class Calculator
+    {
+        //Solution that doesn't work
+        public static void Triple(int x)
+        {
+            x = x * 3;
+        }
+    }
+}
---
+++ Calculator_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculator.Triple(a);
+            Console.WriteLine(a);
+        }
+    }
+}
