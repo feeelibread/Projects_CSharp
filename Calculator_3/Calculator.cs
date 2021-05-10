c6851ae50e7bf66a436d06b0617483a8115b9fe9
feeelibread <feeelpoos@gmail.com>
2021-05-10 17:47:56 -0300

    Similar to the modifier "ref" (makes the parameter be a reference to the original variable), buuuuut it doens't need to the original varible to be initiated.

---
+++ Calculator_3/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator_3
+{
+    class Calculator
+    {
+        public static void Triple(int x, out int result)
+        {
+            result = x * 3;
+        }
+    }
+}
---
+++ Calculator_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int result;
+            Calculator.Triple(a, out result);
+            Console.WriteLine(result);
+        }
+    }
+}
