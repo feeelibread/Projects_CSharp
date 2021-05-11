f42eef34a4a19c7110f54663a0b20149b9e3e4e3
feeelibread <feeelpoos@gmail.com>
2021-05-11 13:00:00 -0300

    That process is called "Boxing" when a struct type turns to be the Reference type (allocated in heap)
    PS.
    Only with compatible types.

---
+++ Boxing_Unboxing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Object obj = x;
+            Console.WriteLine(obj);
+
+        }
+    }
+}
