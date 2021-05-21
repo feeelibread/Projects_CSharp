90254172ec38ac913ddc1972a25b06e443aa29a9
feeelibread <feeelpoos@gmail.com>
2021-05-21 12:33:21 -0300

    An exercise about List types, just to make sure that I'm learning.
    PS.
    I got help...

---
+++ List_Exercise/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace List_Exercise
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
---
+++ List_Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(new Employee(id: 1, name: "Roberto", salary: 1000.0));
+            employees.Add(new Employee(id: 2, name: "John", salary: 1050.0));
+            employees.Add(new Employee(id: 3, name: "Aquele cara lá", salary: 998.0));
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine($"ID: {employees[i].Id}, Name: {employees[i].Name}, Salary: {employees[i].Salary.ToString("C2")}");
+            }
+        }
+    }
+}
