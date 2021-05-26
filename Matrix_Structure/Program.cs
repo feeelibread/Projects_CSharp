f12091cbd7fb7ef330275c14213b83b6139d31e6
feeelibread <feeelpoos@gmail.com>
2021-05-26 11:38:35 -0300

    Basics of matrix structure

---
+++ Matrix_Structure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Matrix_Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine("Length - " + mat.Length);
+            Console.WriteLine("Rank - " + mat.Rank);
+            Console.WriteLine("Get length (0) - " + mat.GetLength(0));
+            Console.WriteLine("Get length (1) - " + mat.GetLength(1));
+        }
+    }
+}
