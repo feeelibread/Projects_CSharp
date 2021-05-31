865415d96ed7f84b684836774fc231ebbb8be71f
feeelibread <feeelpoos@gmail.com>
2021-05-31 16:02:47 -0300

    Review about the matrix structure

---
+++ Matrix_Structure_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Matrix_Structure_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"\nNegative numbers: {count}");
+
+        }
+    }
+}
