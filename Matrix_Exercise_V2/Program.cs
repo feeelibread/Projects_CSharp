0332eaf92f8b62f53a96a1d207d5441fa5f9ae3b
feeelibread <feeelpoos@gmail.com>
2021-06-07 10:38:31 -0300

    Matrix exercises, i was a bit confuse with (j < n - 1), but i will understand that part soon

---
+++ Matrix_Exercise_V2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Matrix_Exercise_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(vet[j]);
+                }
+            }
+            
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (x == mat[i, j])
+                    {
+                        Console.WriteLine($"Position {i},{j}:");
+                        if(j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if(i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if(j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
