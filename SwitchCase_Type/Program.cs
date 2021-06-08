6da36e25949553815a621222e561eee8939dae5b
feeelibread <feeelpoos@gmail.com>
2021-06-08 09:42:30 -0300

    Studies about Switch case and Var types

---
+++ SwitchCase_Type/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchCase_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Convert.ToInt32(Console.ReadLine());
+            int a = 10;
+            int b = 5;
+            switch (x)
+            {
+                case 1:
+                    a = Add(a, b);
+                    break;
+                case 2:
+                    a = Sub(a, b);
+                    break;
+            }
+
+            Console.WriteLine(a);
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
---
+++ Var_Type/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Var_Type
+{
+    class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var churros = IsShibata();
+            var churrosComposto = IsShibataComposta();
+            var x = 10;
+            var y = 1.0;
+
+        }
+
+        static bool IsShibata()
+        {
+            return true;
+        }
+
+        static Pessoa IsShibataComposta()
+        {
+            return new Pessoa();
+        }
+    }
+}
