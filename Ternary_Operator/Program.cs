1c1cf3576fe05497623d58b4f377e6045ba994f5
feeelibread <feeelpoos@gmail.com>
2021-06-10 12:43:11 -0300

    Learning about teranary operators, it seems pretty easier

---
+++ Ternary_Operator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ternary_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double descount;
+
+            //"if" operator
+            if (price < 20.0)
+            {
+                descount = price * 0.1;
+            }
+            else
+            {
+                descount = price * 0.05;
+            }
+
+            Console.WriteLine(descount);
+
+            //the ternary operator, in a single line we could do the same as if and else
+            double ternary_descount = (price < 20.0) ? price * 0.1 : price * 0.05;
+            Console.WriteLine(ternary_descount);
+
+        }
+    }
+}
