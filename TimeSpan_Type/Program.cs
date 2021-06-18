ce8df11cda9bdb9a71febcd8a28a6d15aa153bc6
feeelibread <feeelpoos@gmail.com>
2021-06-18 16:37:47 -0300

    TimeSpan, it's the duration of time good for calculate

---
+++ TimeSpan_Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TimeSpan_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            TimeSpan t2 = new TimeSpan(900000000L);
+            Console.WriteLine(t2);
+
+            TimeSpan t3 = new TimeSpan(1, 2, 40, 59);
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = TimeSpan.FromDays(1.5);
+            TimeSpan t5 = TimeSpan.FromHours(1.5);
+            TimeSpan t6 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t7 = TimeSpan.FromSeconds(1.5);
+
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+
+        }
+    }
+}
