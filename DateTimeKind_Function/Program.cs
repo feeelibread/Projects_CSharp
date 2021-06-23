df4f6d6b19ac408604acb6a9edb524ae0d3eb990
feeelibread <feeelpoos@gmail.com>
2021-06-23 12:23:51 -0300

    DateTime kind with pattern ISO8601, it's very useful and a convention with date times, it's good to remember

--- DateTimeKind_Function/Program.cs
+++ DateTimeKind_Function/Program.cs
@@ -18,7 +18,18 @@ namespace DateTimeKind_Function
             Console.WriteLine("dateTime Kind: " + dateTime.Kind);
             Console.WriteLine("dateTme to Local: " + dateTime.ToLocalTime());
             Console.WriteLine("dateTime to UTC: " + dateTime.ToUniversalTime());
-            
+
+            DateTime dateTime3 = DateTime.Parse("2021-06-23T09:40:35Z");
+            DateTime dateTime4 = DateTime.Parse("2021-06-23 09:40:35");
+            Console.WriteLine("\nDate time pattern ISO8601: " + dateTime3);
+            Console.WriteLine("Date time withou the pattern: " + dateTime4);
+
+            Console.WriteLine("\nDate Time Local: " + dateTime3.ToLocalTime());
+            Console.WriteLine("Date Time kind: " + dateTime3.Kind);
+            Console.WriteLine("Date Time UTC: " + dateTime3.ToUniversalTime());
+            Console.WriteLine("\nDate Time mask: " + dateTime3.ToString("yyyy-MM-ddTHH:mm:ssZ")); //don't do it
+            Console.WriteLine("Date Time mask (the best way): " + dateTime3.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
         }
     }
 }
