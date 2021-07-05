333173fee72619e97f79a19e30186d9e273a0094
feeelibread <feeelpoos@gmail.com>
2021-07-05 14:03:35 -0300

    I'm learning about inheritance, it's very useful.

---
+++ Inheritance_OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿using Inheritance_OOP.Entities;
+using System;
+
+namespace Inheritance_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1008, "Felipe", 1200.00, 200);
+            Console.WriteLine(account.Balance);
+            
+        }
+    }
+}
