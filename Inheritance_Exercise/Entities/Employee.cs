02a75390314b3512bd3e3fe2f2f368662dd3bdf4
feeelibread <feeelpoos@gmail.com>
2021-07-12 10:45:21 -0300

    An exercise with inheritance and polymorphism, it was very nice doing. I think that im learning well

---
+++ Inheritance_Exercise/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Exercise.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
---
+++ Inheritance_Exercise/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Exercise.Entities
+{
+    sealed class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additonalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additonalCharge;
+        }
+
+        public sealed override double Payment()
+        {
+            double percentage = 1.1;
+            return base.Payment() + AdditionalCharge * percentage;
+
+        }
+    }
+}
