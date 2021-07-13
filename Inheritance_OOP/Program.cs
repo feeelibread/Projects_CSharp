e83c51c9c3faa7df490cfdfe719eb0ab9fbb58ff
feeelibread <feeelpoos@gmail.com>
2021-07-13 12:12:00 -0300

    I turned account into an Abstract Class, just to learn how to use it

--- Inheritance_OOP/Program.cs
+++ Inheritance_OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Inheritance_OOP.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Inheritance_OOP
 {
@@ -7,48 +8,32 @@ namespace Inheritance_OOP
     {
         static void Main(string[] args)
         {
-            Casting();
-            OverrideAndVirtual();
+            //An abstract class cannot be instantianted, it works like a mold for the classes
+            //SavingsAccount and BusinessAccount
+            //Account acc1 = new Account(1001, "Berto", 1500.00);
+            List<Account> acc = new List<Account>();
+            acc.Add(new SavingsAccount(1002, "Robert", 1350.50, 0.50));
+            acc.Add(new BusinessAccount(1003, "Robert", 1350.50, 200.00));
+            acc.Add(new SavingsAccount(1004, "Pale", 1350.50, 0.50));
+            acc.Add(new BusinessAccount(1005, "L", 1350, 10.0));
 
-        }
-
-        private static void OverrideAndVirtual()
-        {
-            Account acc = new Account(1002, "Allan", 500.00);
-            Account acc2 = new SavingsAccount(1003, "Robert", 500.00, 0.0);
-            acc.Withdraw(100);
-            acc2.Withdraw(100);
-            Console.WriteLine(acc.Balance);
-            Console.WriteLine(acc2.Balance);
-        }
-
-        private static void Casting()
-        {
-            Account acc = new Account(1001, "James", 1200.00);
-            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 3000.00, 200.0);
-
-            //UPCASTING - Converting a subclass to a superclass. This process is called Upcast
-            Account acc1 = bacc;
-            Account acc2 = new BusinessAccount(1003, "Hodor", 1500.0, 150.0);
-            Account acc3 = new SavingsAccount(1004, "Alana", 1300.0, 250.0);
-
-            //DOWNCASTING - Conerting a superclass to a subclass. This process is called Downcast
-            BusinessAccount acc4 = (BusinessAccount)acc2;
-            acc4.Loan(10);
-
-            if (acc3 is BusinessAccount)
+            double sum = 0.0;
+            foreach (Account item in acc)
             {
-                BusinessAccount acc5 = acc3 as BusinessAccount;
-                acc5.Loan(150.0);
-                Console.WriteLine("Loan!");
+                sum += item.Balance;
+            }
+            Console.WriteLine($"Total balance: {sum}");
+
+            foreach (Account item in acc)
+            {
+                item.Withdraw(100);
+                Console.WriteLine($"\nUpated balance {item.Number}: {item.Balance.ToString("C2")}");
             }
 
-            if (acc3 is SavingsAccount)
-            {
-                SavingsAccount acc5 = acc3 as SavingsAccount;
-                acc5.UpdateBalance();
-                Console.WriteLine("Update!");
-            }
+
         }
+
+
     }
 }
+
