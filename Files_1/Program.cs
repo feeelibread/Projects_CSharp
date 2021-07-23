bb3370b67c6ea51536deac9537d9889e85e3dceb
feeelibread <feeelpoos@gmail.com>
2021-07-23 10:45:21 -0300

    Learning about how C# work with files

---
+++ Files_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Files_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\Public\file1.txt";
+            string targetPath = @"C:\Users\Public\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(targetPath);
+                foreach (string item in lines)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
