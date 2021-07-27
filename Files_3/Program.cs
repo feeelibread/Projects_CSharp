10e3a57d6aa624b843212ac2df4304f00c586ade
feeelibread <feeelpoos@gmail.com>
2021-07-27 11:59:48 -0300

    block using, to reduce the code and making it more "readable"

---
+++ Files_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Files_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\temp\file1.txt";
+
+            try
+            {
+                using FileStream fs = new FileStream(path, FileMode.Open);
+                using StreamReader sr = new StreamReader(fs);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error has ocurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
