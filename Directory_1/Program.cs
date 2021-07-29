56691bdccb7738eaa86c85a848c89b002afab2bb
feeelibread <feeelpoos@gmail.com>
2021-07-29 15:20:46 -0300

    Listing the folders using IEnumerable

---
+++ Directory_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\folder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                foreach (string item in folders)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error has ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
