0371009c62ad9b069b8bc12746536fd77ddfb1a8
feeelibread <feeelpoos@gmail.com>
2021-07-30 10:57:27 -0300

    Exercise using all that i've learned until now (actually i looked at the resolution of it, i'll review the lessons and try again)

---
+++ Files_Exercise/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Files_Exercise.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
+
+        public Product(string name, double price, double quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Files_Exercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Files_Exercise.Entities;
+using System.Globalization;
+using System.IO;
+
+namespace Files_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using StreamWriter sw = File.AppendText(targetFilePath);
+                foreach (var item in lines)
+                {
+                    string[] fields = item.Split(';');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    double quantity = int.Parse(fields[2]);
+
+                    Product p = new Product(name, price, quantity);
+
+                    sw.WriteLine(p.Name + ';' + p.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                }
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
