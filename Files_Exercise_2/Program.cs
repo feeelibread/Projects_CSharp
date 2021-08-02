c56da751a33c7c6f9ec8a0c305565c9f45e5f6b0
feeelibread <feeelpoos@gmail.com>
2021-08-02 11:39:13 -0300

    i redid this exercise again, to make sure that i learned

---
+++ Files_Exercise_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Files_Exercise_2.Entities;
+
+namespace Files_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                
+                string sourceDirPath = Path.GetDirectoryName(sourceFilePath);
+                string targetDirPath = sourceDirPath + @"\out";
+                string targetFilePath = targetDirPath + @"\summary.csv";
+                Directory.CreateDirectory(targetDirPath);
+
+                using StreamWriter sw = File.AppendText(targetFilePath);
+                foreach (var item in lines)
+                {
+                    string[] v = item.Split(';');
+                    string name = v[0];
+                    double price = double.Parse(v[1],CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(v[2]);
+                    Product p = new Product(name, price, quantity);
+
+                    sw.WriteLine(p.Name + ';' + p.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                }
+
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error has ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
