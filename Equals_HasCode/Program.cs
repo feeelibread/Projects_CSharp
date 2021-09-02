0f16245ee98a507faed8f8671c0d6011241719f5
feeelibread <feeelpoos@gmail.com>
2021-09-02 10:04:21 -0300

    About GetHashCode and Equals functions

--- Equals_HasCode/Program.cs
+++ Equals_HasCode/Program.cs
@@ -6,7 +6,15 @@ namespace Equals_HasCode
     {
         static void Main(string[] args)
         {
-           
+            string a = "maria";
+            string b = "roberto";
+
+            Console.WriteLine(a.Equals(b));
+
+            //If the code of objects is different, probably they are different. It has a chance do "collide"
+            //and generate the same hash code for different objects, but hardly ever happens
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
         }
     }
 }
