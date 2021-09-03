1993aa32cee4305056c360a0bce88e18bd0363d1
feeelibread <feeelpoos@gmail.com>
2021-09-03 10:59:27 -0300

    HashSet demonstration

---
+++ HashSet_SortedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet_SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Notebook");
+            set.Add("TV");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+        }
+    }
+}
