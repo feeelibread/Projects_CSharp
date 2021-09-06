9f02362bde815441530731e744c24cc323c9b157
feeelibread <feeelpoos@gmail.com>
2021-09-06 09:37:05 -0300

    Union, intersection  and difference using sortedset

--- HashSet_SortedSet/Program.cs
+++ HashSet_SortedSet/Program.cs
@@ -7,18 +7,32 @@ namespace HashSet_SortedSet
     {
         static void Main(string[] args)
         {
-            HashSet<string> set = new HashSet<string>();
-            set.Add("Notebook");
-            set.Add("TV");
-            set.Add("Tablet");
-            Console.WriteLine(set.Contains("Notebook"));
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() {3, 5, 6, 7, 8, 9, 10 };
 
-            foreach (var item in set)
+            //union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            //intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            //difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
             {
-                Console.WriteLine(item);
+                Console.Write(obj + " ");
             }
-
-            
+            Console.WriteLine();
         }
     }
 }
