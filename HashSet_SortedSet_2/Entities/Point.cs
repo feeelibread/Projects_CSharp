b4b2598ca6ed4ff2121711afd08581a5913355df
feeelibread <feeelpoos@gmail.com>
2021-09-20 11:10:17 -0300

    Understanding how HashSet compare values and objects

---
+++ HashSet_SortedSet_2/Entities/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashSet_SortedSet_2.Entities
+{
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ HashSet_SortedSet_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using HashSet_SortedSet_2.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace HashSet_SortedSet_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> p = new HashSet<Product>();
+            p.Add(new Product("Notebook", 1200.00));
+            p.Add(new Product("Tablet", 900.00));
+
+            HashSet<Point> point = new HashSet<Point>();
+            point.Add(new Point(1, 2));
+            point.Add(new Point(3, 4));
+
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(p.Contains(prod));
+
+            Point points = new Point(3, 4);
+            Console.WriteLine(point.Contains(points));
+
+
+        }
+    }
+}
