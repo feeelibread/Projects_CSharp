786f2624c222db3893e29ec3e430f48e7cc2c8fd
feeelibread <feeelpoos@gmail.com>
2021-09-22 11:34:16 -0300

    An resolved exercise about HashSet and how it works

---
+++ HashSet_Exercise/Entities/LogRecord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashSet_Exercise.Entities
+{
+    class LogRecord
+    {
+        public string Name { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string name, DateTime instant)
+        {
+            Name = name;
+            Instant = instant;
+        }
+
+        public LogRecord()
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return !(obj is LogRecord)
+                ? false
+                : obj is LogRecord record &&
+                   Name == record.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+    }
+}
---
+++ HashSet_Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HashSet_Exercise.Entities;
+
+namespace HashSet_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> logs = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+
+
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] lines = sr.ReadLine().Split(' ');
+                    string username = lines[0];
+                    DateTime instant = DateTime.Parse(lines[1]);
+                    logs.Add(new LogRecord(username, instant));
+                }
+
+                Console.WriteLine($"Total users: {logs.Count}");
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
