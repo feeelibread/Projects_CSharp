5816bf8092fb9ff953793d4d5337226f72f31a16
feeelibread <feeelpoos@gmail.com>
2021-09-23 12:56:03 -0300

    Another resolved exercise, to make sure that i learned

---
+++ HashSet_Exercise_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> hash = new HashSet<int>();
+
+            Console.Write("How many studens for course A: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int students = int.Parse(Console.ReadLine());
+                hash.Add(students);
+            }
+
+            Console.Write("How many studens for course B: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int students = int.Parse(Console.ReadLine());
+                hash.Add(students);
+            }
+            
+            Console.Write("How many studens for course C: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int students = int.Parse(Console.ReadLine());
+                hash.Add(students);
+            }
+
+            Console.WriteLine($"Total students: {hash.Count}");
+        }
+    }
+}
