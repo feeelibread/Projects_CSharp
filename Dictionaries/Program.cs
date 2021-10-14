416ee5cd9ad2fe4aeeb8ac941062026dab9516ee
feeelibread <feeelpoos@gmail.com>
2021-10-14 15:16:58 -0300

    Exercise about dictionaries

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "991263548";
+            cookies["phone"] = "991542135";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("The given key 'email' was not present in the dictionary.");
+            }
+
+            Console.WriteLine($"Size: {cookies.Count}");
+            Console.WriteLine("All cookies:");
+            
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ Dictionaries_Exercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dictionaries_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the file path: ");
+            string path = Console.ReadLine();
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            try
+            {
+
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] candidates = sr.ReadLine().Split(';');
+                    string name = candidates[0];
+                    int votes = int.Parse(candidates[1]);
+                    
+                    if (dictionary.ContainsKey(name))
+                    {
+                        dictionary[name] += votes;
+                    }
+                    else
+                    {
+                        dictionary[name] = votes;
+                    }
+                }
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+                
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error has ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
