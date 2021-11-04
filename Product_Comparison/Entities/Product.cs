76d0edd6540385f3ed3ba04ab34f1617e7fc1b94
feeelibread <feeelpoos@gmail.com>
2021-11-04 13:06:46 -0300

    IComparable implementation

---
+++ Product_Comparison/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text;
+
+namespace Product_Comparison.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("C2")}";
+        }
+
+        public int CompareTo([AllowNull] Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ Product_Comparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using Product_Comparison.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Product_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 1500.00));
+            list.Add(new Product("Alexa", 350.00));
+            list.Add(new Product("Notebook", 8900.00));
+
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
