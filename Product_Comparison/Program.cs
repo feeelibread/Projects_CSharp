9e4010b8c4cec3a97b4f5d6548796f577e57e7f4
feeelibread <feeelpoos@gmail.com>
2021-11-04 14:39:05 -0300

    Method reference as a parameter

--- Product_Comparison/Program.cs
+++ Product_Comparison/Program.cs
@@ -13,11 +13,17 @@ namespace Product_Comparison
             list.Add(new Product("Alexa", 350.00));
             list.Add(new Product("Notebook", 8900.00));
 
-            list.Sort();
+            list.Sort(CompareProducts);
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
         }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        }
     }
+
 }
