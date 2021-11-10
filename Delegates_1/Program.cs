20a75a94b0787f54899741a4411af807ea6326de
feeelibread <feeelpoos@gmail.com>
2021-11-10 11:30:27 -0300

    A operation made withou delegates

---
+++ Delegates_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Delegates_1.Services;
+
+double a = 10;
+double b = 5;
+
+double result = CalculationService.Max(a, b);
+Console.WriteLine(result);
+
+result = CalculationService.Sum(a, b);
+Console.WriteLine(result);
+
+CalculationService.Square(a);
+Console.WriteLine(result);
+
