5d49581326a474b9a3fec358d2090402e949debb
feeelibread <feeelpoos@gmail.com>
2021-11-17 12:04:44 -0300

    A way to remove the products that the price is above 100.00

---
+++ Predicate_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Predicate_1.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Predicate_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Table", 349.99));
+            list.Add(new Product("HD Case", 80.00));
+
+            list.RemoveAll(p => p.Price >= 100.00);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            
+            
+        }
+    }
+}
