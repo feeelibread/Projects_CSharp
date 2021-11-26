a62140edd02b369354b876024e0ce12b981bca5b
feeelibread <feeelpoos@gmail.com>
2021-11-26 09:21:38 -0300

    How LINQ works

---
+++ Linq_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the LINQ, it has many options for consulting.
+            //To work with it, this process usually involves 3 steps.
+
+
+            //Data source = where the data comes from
+            int[] numbers = new int[] { 2, 3, 4, 5, 6 };
+
+            //Query expression
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10).ToList();
+
+            //Executing the query
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
